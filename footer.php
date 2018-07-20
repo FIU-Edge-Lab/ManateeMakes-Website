7f935d8a2758ad1ba0721223f1e9bd3939e76ce9
Guillermo Garrido <GuillermoGarrido@users.noreply.github.com>
2018-07-20 11:50:24 -0400

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,13 @@
+<footer id="colophon" class="rsrc-footer" role="contentinfo">
+	<div class="container"> 
+		<!-- old credits went here -->
+	</div>       
+</footer> 
+<p id="back-top">
+	<a href="#top"><span></span></a>
+</p>
+<!-- end main container -->
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
